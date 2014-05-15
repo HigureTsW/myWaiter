fa190f106804460ffa8f3e50a89db346dd512f11
pusulit <s.perga@gmail.com>
2014-05-15 20:05:23 +0200

    version1 with maps

--- myWaiter/src/it/sii/mywaiter/MapActivity.java
+++ myWaiter/src/it/sii/mywaiter/MapActivity.java
@@ -1,15 +1,54 @@
 package it.sii.mywaiter;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.google.android.gms.maps.MapView;
 
-public class MapActivity extends Activity {
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
+
+public class MapActivity extends FragmentActivity  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map_layout);
+		
 	}
 
 }
+
+
+
+/*public class MapActivity extends Activity {
+
+	MapView mMapView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map_layout);
+	
+		mMapView = (MapView) findViewById(R.layout.map_layout);
+    	mMapView.onCreate(savedInstanceState);
+    
+	}
+
+	@Override
+	public void onPause() {
+    	super.onPause();
+    	mMapView.onPause();
+	}
+
+	@Override
+	public void onResume() {
+   		super.onResume();
+   		mMapView.onResume();
+	}
+
+	@Override
+	public void onDestroy() {
+    	super.onDestroy();
+    	mMapView.onDestroy();
+	}	
+
+}*/
